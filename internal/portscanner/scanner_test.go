f16e1cfad65114366939e1ba0596ae93400d2a08
Lambert Dodd <95171LambertDodd@users.noreply.github.com>
2026-04-16 01:04:22 +0800

    test: add TestParseProcNet_MultipleListenPorts to cover multiple LISTEN entries

--- internal/portscanner/scanner_test.go
+++ internal/portscanner/scanner_test.go
@@ -82,6 +82,32 @@ func TestParseProcNet_EmptyFile(t *testing.T) {
 	}
 }
 
+func TestParseProcNet_MultipleListenPorts(t *testing.T) {
+	// Both entries are in LISTEN state (0A); both should be returned
+	content := `  sl  local_address rem_address   st tx_queue rx_queue tr tm->when retrnsmt   uid  timeout inode
+   0: 00000000:1F90 00000000:0000 0A 00000000:00000000 00:00000000 00000000     0        0 12345 1 0000000000000000 100 0 0 10 0
+   1: 00000000:0050 00000000:0000 0A 00000000:00000000 00:00000000 00000000     0        0 67890 1 0000000000000000 100 0 0 10 0
+`
+	tmpFile, err := os.CreateTemp("", "proc_net_tcp_multi_*")
+	if err != nil {
+		t.Fatalf("failed to create temp file: %v", err)
+	}
+	defer os.Remove(tmpFile.Name())
+
+	if _, err := tmpFile.WriteString(content); err != nil {
+		t.Fatalf("failed to write temp file: %v", err)
+	}
+	tmpFile.Close()
+
+	entries, err := parseProcNet(tmpFile.Name())
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if len(entries) != 2 {
+		t.Fatalf("expected 2 entries, got %d", len(entries))
+	}
+}
+
 func TestScan_ReturnsNoError(t *testing.T) {
 	s := NewScanner()
 	// Scan should not return an error even if /proc/net/tcp doesn't exist
